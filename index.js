88bc6723bc880533b80c7bec5784c65db79193e7
jgrodriguez9 <jgrodriguez.pro@gmail.com>
2023-07-07 15:39:58 -0500

    test 2

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-/**
- * @license
- * Copyright 2019 Google LLC. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-// [START maps_add_map]
 // Initialize and add the map
 let map;
 
 async function initMap() {
-  // [START maps_add_map_instantiate_map]
   // The location of Uluru
   const position = { lat: -25.344, lng: 131.031 };
   // Request needed libraries.
@@ -23,16 +16,12 @@ async function initMap() {
     mapId: "DEMO_MAP_ID",
   });
 
-  // [END maps_add_map_instantiate_map]
-  // [START maps_add_map_instantiate_marker]
   // The marker, positioned at Uluru
   const marker = new AdvancedMarkerView({
     map: map,
     position: position,
     title: "Uluru",
   });
-  // [END maps_add_map_instantiate_marker]
 }
 
 initMap();
-// [END maps_add_map]
